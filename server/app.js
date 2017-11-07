87bb0b2d5ac144fe3edeaecf4fb93a6d11d14a3c
Ryan Spencer <rxs2833@g.rit.edu>
2017-11-07 15:47:19 -0500

    fixed typo

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ let redisPASS;
 
 if (process.env.REDISCLOUD_URL) {
   redisURL = url.parse(process.env.REDISCLOUD_URL);
-  redisPASS = redisURL = redisURL.auth.split(':')[1];
+  redisPASS = redisURL.auth.split(':')[1];
 }
 
 const router = require('./router.js');
